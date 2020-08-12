42524d9caf6f8e479354b90293cc19fe4f3b2efa
hhidk <295809364@qq.com>
2020-08-12 15:08:11 +0800

    UserSpace框架

--- DiamondDocs/src/main/java/com/diamond/controller/UserSpaceController.java
+++ DiamondDocs/src/main/java/com/diamond/controller/UserSpaceController.java
@@ -1,9 +1,53 @@
 package com.diamond.controller;
 
+import com.diamond.dto.DocPreview;
+import com.diamond.service.UserSpaceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*",maxAge = 3600)
 public class UserSpaceController {
+
+    @Autowired
+    private UserSpaceService userSpaceService;
+
+    @RequestMapping("/getMyDocs")
+    public List<DocPreview> getMyDocs(int userID){
+        try {
+            return userSpaceService.getMyDocs(userID);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @RequestMapping("/getRecentDocs")
+    public List<DocPreview> getRecentDocs(int userID){
+        try {
+            return userSpaceService.getRecentDocs(userID);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @RequestMapping("/getFavoriteDocs")
+    public List<DocPreview> getFavoriteDocs(int userID){
+        try {
+            return userSpaceService.getFavoriteDocs(userID);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
--- DiamondDocs/src/main/java/com/diamond/service/UserSpaceService.java
+++ DiamondDocs/src/main/java/com/diamond/service/UserSpaceService.java
@@ -1,7 +1,23 @@
 package com.diamond.service;
 
+import com.diamond.dto.DocPreview;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserSpaceService {
+
+    public List<DocPreview> getMyDocs(int userID) throws Exception{
+        return null;
+    }
+
+    public List<DocPreview> getRecentDocs(int userID) throws Exception{
+        return null;
+    }
+
+    public List<DocPreview> getFavoriteDocs(int userID) throws Exception{
+        return null;
+    }
+
 }
