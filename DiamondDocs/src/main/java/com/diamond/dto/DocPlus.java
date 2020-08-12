08dc719938275b1dbca5e58a38e0c08a8ede9d70
hhidk <295809364@qq.com>
2020-08-12 15:25:53 +0800

    Doc框架，未完成版

---
+++ DiamondDocs/src/main/java/com/diamond/dto/DocPlus.java
@@ -0,0 +1,10 @@
+package com.diamond.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DocPlus {
+    //具体有啥得看前端的需求
+}
--- DiamondDocs/src/main/java/com/diamond/service/DocService.java
+++ DiamondDocs/src/main/java/com/diamond/service/DocService.java
@@ -1,7 +1,14 @@
 package com.diamond.service;
 
+import com.diamond.dto.DocPlus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocService {
+
+    public DocPlus getDoc(int docID) {
+        return null;
+    }
+
+
 }
