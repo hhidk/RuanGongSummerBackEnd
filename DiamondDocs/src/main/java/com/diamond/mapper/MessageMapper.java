d3bb995c41ee2e2a5d5f0a02ac47e8e84ed96f90
hhidk <295809364@qq.com>
2020-08-13 09:56:09 +0800

    Msg

---
+++ DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
@@ -0,0 +1,9 @@
+package com.diamond.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MessageMapper {
+}
