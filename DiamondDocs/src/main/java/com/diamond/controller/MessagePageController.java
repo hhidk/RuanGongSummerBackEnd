c9603e1ea6f7b5099dff59b8bd62813225a40d1a
luan6ma <1537011738@qq.com>
2020-08-13 14:45:11 +0800

    完成了message的api

--- DiamondDocs/src/main/java/com/diamond/controller/MessagePageController.java
+++ DiamondDocs/src/main/java/com/diamond/controller/MessagePageController.java
@@ -1,9 +1,46 @@
 package com.diamond.controller;
 
+import com.diamond.dto.TeamMessage;
+import com.diamond.mapper.MessageMapper;
+import com.diamond.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*",maxAge = 3600)
 public class MessagePageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @RequestMapping("/getCommonMsg")
+    public List<TeamMessage> getCommonMsg(@RequestParam("type") int type, @RequestParam("userID") String userID)
+    {
+        try {
+            return messageService.getCommonMsg(type, userID);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @RequestMapping("/deleteMsg")
+    public int deleteMsg(@RequestParam("msgID") String msgID)
+    {
+        try {
+            messageService.deleteMsg(msgID);
+            return 0;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
 }
