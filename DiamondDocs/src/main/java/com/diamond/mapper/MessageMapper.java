7658b478a177ca4523552938bac6399452119fc3
luan6ma <1537011738@qq.com>
2020-08-13 16:38:18 +0800

    完成了申请的相关service内容

--- DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
+++ DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
@@ -1,6 +1,7 @@
 package com.diamond.mapper;
 
 import com.diamond.dto.TeamMessage;
+import com.diamond.pojo.Message;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,8 @@ import java.util.List;
 @Mapper
 @Repository
 public interface MessageMapper {
+
+    int addMessage(Message message);
 
     List<TeamMessage> getInvitationMsg(@Param("userID") String userID);
 
--- DiamondDocs/src/main/java/com/diamond/service/MessageService.java
+++ DiamondDocs/src/main/java/com/diamond/service/MessageService.java
@@ -2,6 +2,9 @@ package com.diamond.service;
 
 import com.diamond.dto.TeamMessage;
 import com.diamond.mapper.MessageMapper;
+import com.diamond.mapper.TeamMapper;
+import com.diamond.pojo.Message;
+import com.diamond.utils.DiyUUID;
 import com.diamond.utils.FormatHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,86 @@ public class MessageService {
 
     @Autowired
     private MessageMapper messageMapper;
+    @Autowired
+    private TeamMapper teamMapper;
+
+    public void applyTeam(String userID, String targetTeamID, String content) throws Exception
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent(content);
+        message.setMsgType(1);
+        message.setTeamID(targetTeamID);
+        message.setUserID(userID);
+        message.setTargetUserID(teamMapper.getCreatorIDByTeamID(targetTeamID));
+
+        messageMapper.addMessage(message);
+    }
+
+    public void accpetMember(String userID, String targetUserID, String teamID)
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent("");
+        message.setMsgType(3);
+        message.setTeamID(teamID);
+        message.setUserID(userID);
+        message.setTargetUserID(targetUserID);
+
+        messageMapper.addMessage(message);
+    }
+
+    public void refuseMember(String userID, String targetUserID, String teamID)
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent("");
+        message.setMsgType(4);
+        message.setTeamID(teamID);
+        message.setUserID(userID);
+        message.setTargetUserID(targetUserID);
+
+        messageMapper.addMessage(message);
+    }
+
+    public void inviteMember(String userID, String teamID, String targetUserID, String content)
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent(content);
+        message.setMsgType(0);
+        message.setTeamID(teamID);
+        message.setUserID(userID);
+        message.setTargetUserID(targetUserID);
+
+        messageMapper.addMessage(message);
+    }
+
+    public void joinTeam(String userID, String targetUserID, String teamID)
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent("");
+        message.setMsgType(5);
+        message.setTeamID(teamID);
+        message.setUserID(userID);
+        message.setTargetUserID(targetUserID);
+
+        messageMapper.addMessage(message);
+    }
+
+    public void refuseTeam(String userID, String targetUserID, String teamID)
+    {
+        Message message = new Message();
+        message.setMsgID(DiyUUID.generateMsgID());
+        message.setMsgContent("");
+        message.setMsgType(6);
+        message.setTeamID(teamID);
+        message.setUserID(userID);
+        message.setTargetUserID(targetUserID);
+
+        messageMapper.addMessage(message);
+    }
 
     public List<TeamMessage> getCommonMsg(int type, String userID) throws Exception
     {
