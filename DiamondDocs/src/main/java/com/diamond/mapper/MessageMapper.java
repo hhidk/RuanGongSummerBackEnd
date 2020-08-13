fbd286860b3576542a23e89ce969bedca53723ed
luan6ma <1537011738@qq.com>
2020-08-13 12:32:14 +0800

    添加了消息相关的mapper

--- DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
+++ DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
@@ -1,12 +1,20 @@
 package com.diamond.mapper;
 
+import com.diamond.dto.TeamMessage;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface MessageMapper {
 
+    List<TeamMessage> getInvitationMsg(@Param("userID") String userID);
+
+    List<TeamMessage> getApplicationMsg(@Param("userID") String userID);
+
     int deleteMessage(@Param("msgID") String msgID);
+
 }
