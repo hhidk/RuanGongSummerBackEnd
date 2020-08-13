4a0f8a7b1812ddbaad748a6c8beb9d9cf715a99a
luan6ma <1537011738@qq.com>
2020-08-13 11:37:13 +0800

    aaa

--- DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
+++ DiamondDocs/src/main/java/com/diamond/mapper/MessageMapper.java
@@ -1,9 +1,12 @@
 package com.diamond.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface MessageMapper {
+
+    int deleteMessage(@Param("msgID") String msgID);
 }
