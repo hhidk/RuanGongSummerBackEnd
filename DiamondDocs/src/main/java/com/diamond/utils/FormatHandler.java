2f5b0ce8df42ca4d41f3bf23c8139535db105b42
hhidk <295809364@qq.com>
2020-08-18 09:35:20 +0800

    时间优化和评论顺序

--- DiamondDocs/src/main/java/com/diamond/utils/FormatHandler.java
+++ DiamondDocs/src/main/java/com/diamond/utils/FormatHandler.java
@@ -10,6 +10,7 @@ public class FormatHandler {
         try {
             SimpleDateFormat sqlFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             Date nowDate=new Date();
+            String nowtime=sqlFormat.format(nowDate);
             Date date=sqlFormat.parse(sqltime);
 
             long l=nowDate.getTime()-date.getTime();
@@ -17,16 +18,14 @@ public class FormatHandler {
             long hour=(l/(60*60*1000)-day*24);
             long min=((l/(60*1000))-day*24*60-hour*60);
             //long second=(l/1000-day*24*60*60-hour*60*60-min*60);
-            if(day>3)
-                return sqltime.substring(0,16);
-            else if(day>0)
-                return day+"天前";
-            else if(hour>0)
-                return hour+"小时前";
-            else if(min>2)
-                return min+"分钟前";
-            else
+            if(day<1 && min<1)
                 return "刚刚";
+            else if(day<1 && min<60)
+                return min+"分钟前";
+            else if(day<1 && hour<24)
+                return hour+"小时"+min+"分钟前";
+            else
+                return sqltime.substring(0,16);
         }
         catch (Exception e){
             e.printStackTrace();
